e41b4b1196f69c4da00901da2391e19081ae188c
Viktor-Gold <viktorzolotko071@gmail.com>
2026-01-29 21:37:22 +0300

    Добавил конфиг Astro для GitHub Pages

---
+++ furniture-site/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Полная ссылка на будущий сайт
+  site: 'https://Viktor-Gold.github.io/ИМЯ_РЕПОЗИТОРИЯ',
+  
+  // Базовый путь (ВАЖНО для GitHub Pages)
+  base: '/furniture_site/',
+});
